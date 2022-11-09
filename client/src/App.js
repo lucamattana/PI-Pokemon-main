2792f8f5c57195922ce721b310adc1482ce40097
lucamattana <luca.mattbes@gmail.com>
2022-11-09 20:36:37 -0300

    started front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <h1>Pokemon by Luca</h1>
+      <Route path='/'>
+        <Nav />
+      </Route>
+      <Route exact path='/'>
+        <Home />
+      </Route>
     </div>
   );
 }
